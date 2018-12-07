6313a474642a2ef08df3a700d2ef5fb969a6c1a8
Adrian Carroll <adrian.carroll7@gmail.com>
2018-12-07 13:16:59 +0000

    Changed entry point

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var Dishes = require(path.resolve(path.dirname(__dirname), "modules/dishes"));
 
 /* GET home page. */
 module.exports = function(router) {
-  router.get('/index.html', function(req, res, next) {
+  router.get('/', function(req, res, next) {
     res.render('index', { 
       dishes: Dishes.get() 
     });
